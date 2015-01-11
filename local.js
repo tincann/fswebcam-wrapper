c65c7f2e56cb5c15355c41b17a57f95151a1df67
tincann <saabnl@gmail.com>
2015-01-11 22:29:57 +0100

    Changed local testing file from video1 to video0

--- local.js
+++ local.js
@@ -1,7 +1,7 @@
 var FsWebcam = require('./lib/fswebcam-wrapper.js');
 
 var camera = new FsWebcam({
-    device: '/dev/video1',
+    device: '/dev/video0',
     rotate: 90,
     title: 'HomeSec',
     subtitle: 'Hallway#1'
